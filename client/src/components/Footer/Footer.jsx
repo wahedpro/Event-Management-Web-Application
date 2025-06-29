6970bde67e747a7996a121d5e8ca61e96002f78c
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-06-29 21:07:33 +0600

    Footer design

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-100 mt-12">
+      <div className="container mx-auto px-4 py-5 ">
+        {/* Copyright */}
+        <div className="text-gray-500 text-sm text-center">
+          © {new Date().getFullYear()} NexusEvents. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router";
 const Navbar = () => {
-  const user = true;
+  const user = false;
   return (
     <nav className="bg-white sticky top-0 z-10 border-b border-gray-200">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
