dd3cd160d690ca165bf55e6bdd46c041d1850e56
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-06-30 19:20:44 +0600

    get all data and show descending order

--- client/src/pages/EventsPage/EventsPage.jsx
+++ client/src/pages/EventsPage/EventsPage.jsx
@@ -1,19 +1,24 @@
-// EventsPage
 import { useEffect, useState } from "react";
 import EventCard from "../../components/EventCard/EventCard";
 
 const EventsPage = () => {
   const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [filterOption, setFilterOption] = useState("");
 
   useEffect(() => {
-    fetch("/events.json")
+    fetch("http://localhost:5000/events")
       .then((res) => res.json())
-      .then((data) => setEvents(data));
+      .then((data) => {
+        setEvents(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
   }, []);
 
-  // search filter
   const filteredEvents = events.filter((event) => {
     const titleMatch = event.title
       .toLowerCase()
@@ -40,13 +45,16 @@ const EventsPage = () => {
     return titleMatch && filterMatch;
   });
 
+  if (loading) {
+    return <div className="text-center py-20">Loading Events...</div>;
+  }
+
   return (
     <div className="max-w-6xl mx-auto p-4">
       <h2 className="text-2xl font-bold mb-6 text-center">All Events</h2>
 
       {/* Search & Filter */}
       <div className="flex flex-col md:flex-row justify-between items-center mb-6 gap-4 border border-gray-200">
-        {/* Search */}
         <input
           type="text"
           placeholder="Search by title..."
@@ -54,8 +62,6 @@ const EventsPage = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           className="border-r border-gray-300 rounded px-3 py-2 w-full md:w-1/3"
         />
-
-        {/* Filter */}
         <select
           value={filterOption}
           onChange={(e) => setFilterOption(e.target.value)}
@@ -68,13 +74,12 @@ const EventsPage = () => {
         </select>
       </div>
 
-      {/* Event Cards */}
       {filteredEvents.length === 0 ? (
         <p className="text-center">No events found.</p>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredEvents.map((event) => (
-            <EventCard key={event.id} event={event} />
+            <EventCard key={event._id} event={event} />
           ))}
         </div>
       )}
